ab50e70903475c87dde72d9a38ecdf5781cc6c32
clacken-dev <lacken596@gmail.com>
2021-11-24 23:16:42 +0000

    Added product app and added to models.py in products app

---
+++ products/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Product(models.Model):
+    service_type = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.service_type
