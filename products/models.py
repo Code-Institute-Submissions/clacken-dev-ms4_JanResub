25e9d442930368c32eef5a51e565a9f22e673960
clacken-dev <lacken596@gmail.com>
2021-11-27 20:32:48 +0000

    Added product_detail.html and individual product view

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Product(models.Model):
     service_type = models.CharField(max_length=254)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(null=True)
 
     def __str__(self):
         return self.service_type
