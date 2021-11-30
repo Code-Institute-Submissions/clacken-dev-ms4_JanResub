bdb683164d4386ef406a9793bb2d8429c0d96246
clacken-dev <lacken596@gmail.com>
2021-11-30 21:08:23 +0000

    create checkout app and model Order

---
+++ checkout/models.py
@@ -0,0 +1,23 @@
+import uuid
+
+from django.db import models
+from products.models import Product
+
+# Create your models here.
+
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    user = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    service = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    description = models.TextField(null=False, blank=False)
+    image = models.ImageField(null=True, blank=True)
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
--- graphic_content/settings.py
+++ graphic_content/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'products',
+    'checkout',
 ]
 
 MIDDLEWARE = [
