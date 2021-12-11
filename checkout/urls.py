eea8b3debafe6f17dab4ddc922453dc364dcb034
clacken-dev <lacken596@gmail.com>
2021-12-11 00:30:47 +0000

    add checkout function, update page styling

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.checkout, name='checkout')
+    
+    path('checkout/<product_id>/', views.checkout, name='checkout'),
+    path('submit_order/', views.submit_order, name='submit_order'),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 from django.contrib import messages
 
 from .forms import OrderForm
 
 
-def checkout(request):
+def checkout(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
 
+    
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'product': product,
     }
 
+    return render(request, template, context)
+
+def submit_order(request):
+
+    form = OrderForm()
+    if request.method == 'POST':
+        print('Printing Post', request.POST)
+
+    template = 'checkout/order_success.html'
+    context = {'form': form}
     return render(request, template, context)
