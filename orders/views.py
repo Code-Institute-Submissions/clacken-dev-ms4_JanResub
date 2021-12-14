135f918a56941dd4294f768deeb5fa5a90d4cb83
clacken-dev <lacken596@gmail.com>
2021-12-14 02:47:02 +0000

    Add profiles templating, update requirements for heroku deployment

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from profiles.models import UserProfile
+from checkout.models import Order
 
 # Create your views here.
+def orders(request):
+    """ A view to return the orders page """
+    profile = get_object_or_404(UserProfile, user=request.user)
+    orders = Order.objects.all()
+
+    template = 'orders/orders.html'
+    context = {
+        'profile': profile,
+        'orders': orders,
+    }
+    
+
+    return render(request, template, context)
