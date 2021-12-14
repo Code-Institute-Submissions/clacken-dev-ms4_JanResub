7df8a9abc6dfb80f2895da1037bd3a2c3e509f81
clacken-dev <lacken596@gmail.com>
2021-12-14 03:35:26 +0000

    remove django secret key

--- graphic_content/settings.py
+++ graphic_content/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ipg2ck-)9o)+-(pc-q0y@_enp1#_p4n5be)^yayva2#p!(clc%'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['clacken-ms4.herokuapp.com', 'localhost']
 
