8370d5ee90e9cec165afb09aae5f3f2586d381c8
clacken-dev <lacken596@gmail.com>
2021-12-15 10:29:01 +0000

    Update 500 error due to checkout.models mistake

--- checkout/models.py
+++ checkout/models.py
@@ -11,7 +11,7 @@ class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
                                      null=True, blank=True,
-                                     related_name='profile')
+                                     related_name='orders')
     date = models.DateTimeField(auto_now_add=True)
     service = models.ForeignKey(Product, null=False, blank=False,
                                 on_delete=models.CASCADE)
--- profiles/views.py
+++ profiles/views.py
@@ -4,7 +4,7 @@ from .models import UserProfile
 from checkout.models import Order
 
 
-@login_required
+
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
