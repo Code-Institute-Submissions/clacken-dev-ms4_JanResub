dfded7ea3115bf5a5ce64a36126b6c0c0ac1695c
clacken-dev <lacken596@gmail.com>
2021-12-15 07:34:25 +0000

    Added user testimonies to products page

--- products/views.py
+++ products/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from orders.models import Testimonial
 
 # Create your views here.
 
 def all_products(request):
     """ A view to show all products """
+    testimonies = Testimonial.objects.all()
 
     products = Product.objects.all()
 
     context = {
         'products': products,
+        'testimonies': testimonies,
     }
 
     return render(request, 'products/products.html', context)
@@ -19,9 +22,10 @@ def product_detail(request, product_id):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
-
+    
     context = {
         'product': product,
+        
     }
 
     return render(request, 'products/product_detail.html', context)
