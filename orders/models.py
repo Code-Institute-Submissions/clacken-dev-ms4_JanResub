96ea3fb8b17a503ead444aaeb87a519842bf5e74
clacken-dev <lacken596@gmail.com>
2022-01-15 16:46:09 +0000

    Updated Testimonial model in orders.models to reflect product purchased and removing foreign key

--- orders/models.py
+++ orders/models.py
@@ -41,8 +41,11 @@ class Testimonial(models.Model):
     """
     A model for testimonials to be shown and drawn from
     """
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    customer_name = models.CharField(max_length=32, null=True)
     testimony = models.TextField(null=False, blank=False)
+    product_purchased = models.ForeignKey(Product, on_delete=models.SET_NULL,
+                                     null=True, blank=True,
+                                     related_name='product')
 
     def __str__(self):
         return self.testimony
