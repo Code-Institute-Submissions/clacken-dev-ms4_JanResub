8fe5b8703df0fb68c694ddff79d89805753f1c57
clacken-dev <lacken596@gmail.com>
2022-01-21 00:30:12 +0000

    Add review model to orders.models

--- checkout/models.py
+++
@@ -1,8 +0,0 @@
-import uuid
-
-from django.db import models
-from products.models import Product
-from profiles.models import UserProfile
-
-# Create your models here.
-
--- products/models.py
+++ products/models.py
@@ -1,10 +1,32 @@
 from django.db import models
+from profiles.models import UserProfile
 
 
 class Product(models.Model):
+    """
+    A model for the products available
+    """
     service_type = models.CharField(max_length=254)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField(null=True)
 
     def __str__(self):
         return str(self.service_type)
+
+
+class Review(models.Model):
+    """
+    A model for the reviews of the workouts.
+    """
+    product = models.ForeignKey(
+        Product, null=True, blank=True, on_delete=models.SET_NULL
+    )
+    user = models.ForeignKey(
+        UserProfile, null=True, blank=True, on_delete=models.CASCADE
+    )
+    review_title = models.CharField(max_length=254, null=True, blank=True)
+    review = models.TextField(blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.review_title)
