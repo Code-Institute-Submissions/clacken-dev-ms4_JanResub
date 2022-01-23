3207aa388bab477612f3e62a3319eac29a6056f5
clacken-dev <lacken596@gmail.com>
2022-01-23 22:07:29 +0000

    Added lead_time, image_url and image fields to Product model

--- products/models.py
+++ products/models.py
@@ -9,7 +9,9 @@ class Product(models.Model):
     service_type = models.CharField(max_length=254)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField(null=True)
-
+    lead_time = models.CharField(max_length=254)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     def __str__(self):
         return str(self.service_type)
 
